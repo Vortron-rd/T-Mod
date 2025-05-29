07c724a1d1c1d0abca110e817b02c87c2706c130
Vortron-rd <119626603+Vortron-rd@users.noreply.github.com>
2025-05-29 16:08:45 -0500

    Update Versioning & other metadata

--- src/main/java/net/vortron/tmod/Reference.java
+++ src/main/java/net/vortron/tmod/Reference.java
@@ -7,6 +7,6 @@ public class Reference {
 
     public static final String MOD_ID = "tmod";
     public static final String MOD_NAME = "T-Mod";
-    public static final String VERSION = "0.0.0";
+    public static final String VERSION = "0.0.1-alpha";
 
 }
